3531cb6df3654a46e347c9723b1420f000e5be27
alex-Arc <alex@arc-light.dk>
2017-02-22 18:22:57 +0100

    add ethernet reset

--- beamSensorVarde.ino
+++ beamSensorVarde.ino
@@ -13,7 +13,19 @@ int outPort = 50001;
 int beam_break_stat = 1;
 #define beam_break_pin 23
 
+#define PIN_RESET 9
+
 void setup() {
+
+  #ifdef PIN_RESET
+  pinMode(PIN_RESET, OUTPUT);
+  digitalWrite(PIN_RESET, LOW);
+  delayMicroseconds(2);
+  digitalWrite(PIN_RESET, HIGH);
+  delay(150);
+  #endif
+
+  pinMode(beam_break_pin, INPUT_PULLUP);
   mac_addr macGet;
   for (int i = 3; i < 6; i++) {
     mac[i] = macGet.m[i];
@@ -35,8 +47,9 @@ void setup() {
 
 void loop() {
   if(digitalRead(beam_break_pin) != beam_break_stat) {
-
+    
     beam_break_stat = digitalRead(beam_break_pin);
+
     //char addr[15];
     //oscAddr.toCharArray(addr, 15);
     char oscStr[23] = {0x2f, 0x42, 0x65, 0x61, 0x6d, 0x42, 0x72, 0x65, 0x61, 0x6b, 0x2f, 0x30, 0x30, 0x30, 0x30, 0x00, 0x2c, 0x69, 0x00, 0x00, 0x00, 0x00, 0x00};
@@ -45,7 +58,7 @@ void loop() {
     char dig2;
     char dig1;
     char dig0;
-    int addrINT = 1;
+    int addrINT = 2;
     dig3 = addrINT/1000;
     dig2 = (addrINT-(dig3*1000))/100;
     dig1 = (addrINT-(dig3*1000)-(dig2*100))/10;
